db524a9b4bb7aafad94bd938ad949d39162ddb9e
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-09 22:43:27 -0500

    made some changes

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
+require('./routes/htmlroutes')(app);
 
 app.listen(PORT, () => {
     console.log(`API server running on port ${PORT} at http://localhost:${PORT} !`);
